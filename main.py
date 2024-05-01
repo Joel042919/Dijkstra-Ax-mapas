a29b0812c6d85a9ce56ba5e2a6ce1c15e7c6523d
Israel Joel Muñoz Rodriguez <israjmr19@gmail.com>
2024-05-01 16:37:41 -0500

    Just format

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import osmnx as ox
-import matplotlib.pyplot as plt 
 
 place_name = "Trujillo, Province of Trujillo, La libertad, Peru"
 
@@ -7,5 +6,4 @@ G = ox.graph_from_place(place_name, network_type="drive")
 
 # show=False para que no se muestr ya que queremos guardarlo
 ox.plot_graph(G, node_size=1,node_color="red",show=False,save=True,filepath="./img/mapa.png")
-
 
